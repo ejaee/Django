5a087860d0b50fcea50d73407fc7eca6b84d6c9f
choiejae <ejae8319@gmail.com>
2023-12-12 18:29:07 +0900

    study: tutorial for django by youtube

---
+++ Django_try/try_1/userapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'userapp'
+urlpatterns = [
+    # ex: /userapp/
+    path("", views.index, name="index"),
+    # ex: /userapp/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /userapp/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /userapp/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
